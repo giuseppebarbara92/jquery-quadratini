09784e73c7b9415b158d9db1af54a7b2bbae10a8
giuseppe.barbara92 <giuseppe.barbara1992@gmail.com>
2018-11-29 18:36:31 +0100

    Correzione esercizio. quadratini + contatore

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+// Consigli su video 34 A
+// Creare una griglia formata da 8x8 quadratini tutti bianchi.
+
+
+
+// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi,
+// gli altri diventano verdi.
+
+var contatore_rossi = 0;
+var contatore_verdi = 0;
+
 $('.square').click(function(){
-$(this).css('background','green');
+var wannabeRed = $(this).hasClass('wannabeRed');
+  if (wannabeRed == true) {
+  $(this).css('background', 'red');
+  contatore_rossi++;
+  $('#number_red').text('Quadrati rossi trovati = ' + contatore_rossi);
+  }
+  else {
+    $(this).css('background', 'green');
+    contatore_verdi++;
+    $('#number_green').html('Quadrati verdi trovati = ' + contatore_verdi);
+  }
 });
 
-$('.red').click(function(){
-$(this).css('background','red');
-});
+
+ // Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
+// wannabeRed
